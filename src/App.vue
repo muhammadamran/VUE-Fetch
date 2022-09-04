524909ddf9505c10fd7f31033e95147332ac4c71
amranhakim10 <amranhakim9@gmail.com>
2022-09-05 02:40:40 +0700

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <Home msg="Vue.js Fetch Data From PHP MySQL"/>
+    <Home/>
   </div>
 </template>
 
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,59 +1,59 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <div class="row">
-      <div class="col-md-6" align="center">
-        <table>
-          <tr>
-            <th>#</th>
-            <th>Username</th>
-            <th>Password</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Gender</th>
+	<div class="header">
+		<h1>My Website</h1>
+		<p>Resize the browser window to see the effect.</p>
+	</div>
+  <hr>
+  <div class="row">
+    <div class="col-md-12" align="center">
+      <table class="table">
+        <tr>
+          <!-- <th>#</th> -->
+          <th>Username</th>
+          <th>Password</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Gender</th>
+        </tr>
+        <tbody>
+          <tr v-for="users in allUsers" v-bind:key="users.id">
+            <td>{{users.username}}</td>
+            <td>{{users.password}}</td>
+            <td>{{users.fname}}</td>
+            <td>{{users.lname}}</td>
+            <td>{{users.gender}}</td>
           </tr>
-        </table>
-      </div>
+        </tbody>
+      </table>
+    </div>
+  </div>
+
+  <div class="for-footer">
+    <div class="footer">
+      <p>Devlopment By @Muhammad Amran &copy; {{ new Date().getFullYear() }}</p>
     </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios' 
+  import "@/assets/style.css";
+	import axios from 'axios' 
 
-export default {
-  name: 'Home',
-  props: {
-    msg: String
-  },
-  data() {
-    return {
-      allUsers:''
-    },
-    mounted() {
-      axios.get('http://127.0.0.1:8080/00.API/vuebackend/backend_api.php').then((res)=> {
-        this.allUsers=res.data
-        console.log(res.data)
-      })
-    }
-  }
-}
+	export default {
+		name: 'Home',
+		props: {
+			msg: String
+		},
+		data() {
+			return {
+				allUsers:''
+			}
+		},
+		mounted() {
+			axios.get('http://127.0.0.1:8080/00.API/vuebackend/backend_api.php').then((res)=> {
+				this.allUsers=res.data
+				console.log(res.data)
+			})
+		}
+	}
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
