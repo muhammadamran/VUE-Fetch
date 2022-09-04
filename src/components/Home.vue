205c051043162b83ef5d9a3e21c601524c3e8547
amranhakim10 <amranhakim9@gmail.com>
2022-09-05 01:53:57 +0700

    Initial commit

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,41 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <div class="row">
+      <div class="col-md-6" align="center">
+        <table>
+          <tr>
+            <th>#</th>
+            <th>Username</th>
+            <th>Password</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Gender</th>
+          </tr>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios' 
+
 export default {
   name: 'Home',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      allUsers:''
+    },
+    mounted() {
+      axios.get('http://127.0.0.1:8080/00.API/vuebackend/backend_api.php').then((res)=> {
+        this.allUsers=res.data
+        console.log(res.data)
+      })
+    }
   }
 }
 </script>
