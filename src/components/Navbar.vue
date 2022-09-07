b661bb38d64a5036b546ab73f90cc0c9f601788a
amranhakim10 <amranhakim9@gmail.com>
2022-09-07 22:19:58 +0700

    Initial commit

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,3 +1,11 @@
 <template>
-    
+    <div class="bg-gray-900 text-gray-100 py-3.5 px-5 shadow md:flex justify-between items-center">
+
+        <div class="flex items-center">
+            <span class="text-green-500 text-xl mr-1">
+                
+            </span>
+        </div>
+
+    </div>
 </template>
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.export = {
+    mode: 'jit',
+
+    purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+
+    drakMode: false,
+
+    theme: {
+        extends: {},
+    },
+
+    variants: {
+        extends: {},
+    },
+
+    plugins: [],
+}
